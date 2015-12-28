02fde372dd005c14626ab0565219f66235611870
Haran Kumar <hkumar8200@gmail.com>
2015-12-28 13:13:30 -0600

    Fixed and documented behavior of arrow keys

--- src/js/main.js
+++ src/js/main.js
@@ -54,6 +54,34 @@ function moveCursorToEnd(mathbox) {
     keystroke(mathbox, 35);
 }
 
+var prevCursorAtBeginning = {}; // TODO: Seriously? Get A Better name than this
+
+function updatePrevCursorAtBeginning(mathbox) {
+    setTimeout(function () {
+        prevCursorAtBeginning[getID(mathbox)] = isCursorAtBeginning(mathbox);
+    }, 10);
+}
+
+function cursorAtBeginningNotChanged(mathbox) {
+    var prev = prevCursorAtBeginning[getID(mathbox)];
+    var curr = isCursorAtBeginning(mathbox);
+    return prev === curr;
+}
+
+var prevCursorAtEnd = {}; // TODO: Seriously? Get A Better name than this
+
+function updatePrevCursorAtEnd(mathbox) {
+    setTimeout(function () {
+        prevCursorAtEnd[getID(mathbox)] = isCursorAtEnd(mathbox);
+    }, 10);
+}
+
+function cursorAtEndNotChanged(mathbox) {
+    var prev = prevCursorAtEnd[getID(mathbox)];
+    var curr = isCursorAtEnd(mathbox);
+    return prev === curr;
+}
+
 /* Text Change Stuff */
 
 var prevText = {};
@@ -74,13 +102,14 @@ function updatePrevText(mathbox) {
 }
 
 function update(mathbox) {
-    // Leaving this wrapper here since I might need to add more update functions
+    updatePrevCursorAtBeginning(mathbox);
+    updatePrevCursorAtEnd(mathbox);
     updatePrevText(mathbox);
 }
 
 // Keyboard Stuff
 
-function onEnter(mathbox){
+function onEnter(mathbox) {
     //TODO: This needs hella refactoring
     /** Adds a new mathbox directly below and focuses it
      * */
@@ -113,7 +142,7 @@ function onEnter(mathbox){
 
 }
 
-function onBackspace(mathbox){
+function onBackspace(mathbox) {
     /** If at the beginning of mathbox, there are more than one, and the backspace wasn't also a text deleting one:
      * Combine the mathbox with the previous one
      * */
@@ -126,7 +155,7 @@ function onBackspace(mathbox){
     }
 }
 
-function onDelete(mathbox){
+function onDelete(mathbox) {
     /** If at the end of mathbox, there are more than one, and the delete wasn't also a text deleting one:
      * Combine the mathbox with the next one
      * */
@@ -139,34 +168,42 @@ function onDelete(mathbox){
     }
 }
 
-//TODO: Arrow Keys Trigger TOO EARLY (i.e. skips a character)
-//TODO: Add Documentation
-
-function onArrowLeft(mathbox){
-    if (isCursorAtBeginning(mathbox)) {
-        var prev = mathbox.prev();
-        if (prev.length !== 0) {
-            focus(prev);
-            moveCursorToEnd(prev);
-        }
+function onArrowLeft(mathbox) {
+    /** If at the beginning of mathbox, there is a box above, and the keystroke didn't cause cursor movement:
+     * Move to mathbox directly above
+     * */
+    var prev = mathbox.prev();
+    if (isCursorAtBeginning(mathbox) && cursorAtBeginningNotChanged(mathbox) && prev.length !== 0) {
+        focus(prev);
+        moveCursorToEnd(prev);
     }
-    update(mathbox);}
+    update(mathbox);
+}
 
-function onArrowRight(mathbox){
+function onArrowRight(mathbox) {
+    /** If at the end of mathbox, there is a box below, and the keystroke didn't cause cursor movement:
+     * Move to mathbox directly below
+     * */
     var next = mathbox.next();
-    if (next.length !== 0) {
+    if (isCursorAtEnd(mathbox) && cursorAtEndNotChanged(mathbox) && next.length !== 0){
         focus(next);
         moveCursorToBeginning(next);
-    }}
+    }
+}
 
-function onArrowUp(mathbox){
+//TODO: This behavior may be annoying when working with sub/superscripts... check if keystroke caused the cursor to move
+
+function onArrowUp(mathbox) {
+    /** If there is a box above, move to it */
     var prev = mathbox.prev();
     if (prev.length !== 0) {
         focus(prev);
         moveCursorToEnd(prev);
-    }}
+    }
+}
 
-function onArrowDown(mathbox){
+function onArrowDown(mathbox) {
+    /** If there is a box below, move to it */
     var next = mathbox.next();
     if (next.length !== 0) {
         focus(next);
@@ -189,7 +226,7 @@ function onKeyDown(e) {
     }
     else if (e.key === "ArrowLeft") {
         onArrowLeft(mathbox);
-    } else if (e.key === "ArrowRight" && isCursorAtEnd(mathbox)) {
+    } else if (e.key === "ArrowRight") {
         onArrowRight(mathbox);
     } else if (e.key === "ArrowUp") {
         onArrowUp(mathbox);
